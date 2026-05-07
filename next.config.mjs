96f2fc8c02d8f64c3af6ddebccce8f3bad14d3e4
Wenhui Zhu <2322145097@qq.com>
2026-05-07 15:20:38 +0800

    强制跳过版本安全检查

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // 加上下面这两段，强制忽略格式和类型检查错误
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
+  eslint: { ignoreDuringBuilds: true },
+  typescript: { ignoreBuildErrors: true },
 };
-
 export default nextConfig;
